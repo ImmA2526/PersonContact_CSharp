fd96d6a93e25358473296fe971541fd9c9e732b3
Avenger2627 <imraninfo97@gmail.com>
2020-12-12 17:28:29 +0530

    [Imran Shaikh] Add: Welcome to Person Contact Management

---
+++ PersonContactMgt/PersonClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonContactMgt
+{
+    public class PersonClass
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+        public PersonClass(string ssn,string name,string address,int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = address;
+            Age=age;
+        }
+    }
+}
---
+++ PersonContactMgt/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PersonContactMgt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonClass> listPersonCity = new List<PersonClass>();
+            Console.WriteLine("*********** Welcome To Person Contact Mgt ***********");
+        }
+    }
+}
