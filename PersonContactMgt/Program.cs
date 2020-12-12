f603e7982668a540705428fc6eae31d351f8720d
Avenger2627 <imraninfo97@gmail.com>
2020-12-12 17:51:32 +0530

    [Imran Shaikh] Add: Create Person Class With Basic Detail

--- PersonContactMgt/Program.cs
+++ PersonContactMgt/Program.cs
@@ -9,6 +9,19 @@ namespace PersonContactMgt
         {
             List<PersonClass> listPersonCity = new List<PersonClass>();
             Console.WriteLine("**** Welcome To Person Contact Mgt ****");
+            AddRecord(listPersonCity);
+        }
+        private static void AddRecord(List<PersonClass> listPersonCity)
+        {
+            listPersonCity.Add(new PersonClass("22223", "Imran", "Adarsh Nagar Pune", 24));
+            listPersonCity.Add(new PersonClass("44453", "Nijam", "Manipura Colony Mumbai", 55));
+            listPersonCity.Add(new PersonClass("45553", "Arti", "Kondhwa Khurd Latur", 22));
+            listPersonCity.Add(new PersonClass("88987", "Pratibha", "Pratibha Villa Mumbai", 18));
+            listPersonCity.Add(new PersonClass("22223", "Soham", "12 main strt Pune", 10));
+            listPersonCity.Add(new PersonClass("44453", "Vidyadhar", "120 main strt Beed", 55));
+            listPersonCity.Add(new PersonClass("45553", "Rehan", "Warali Mumbai", 22));
+            listPersonCity.Add(new PersonClass("88987", "Sumit", "112 main street Latur", 18));
+            Console.WriteLine();
         }
     }
 }
